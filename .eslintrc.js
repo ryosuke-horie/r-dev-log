29e67447c7267c6ba5eca38c470ed791a2e84de7
ryosuke-horie <ryosuke03140@gmial.com>
2023-03-17 08:54:49 +0900

    動作確認を兼ねた、ESLint&Prettierによる自動修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    rules: {
-      // 任意のルールをここに追加できます。
-      // 例: '@typescript-eslint/explicit-function-return-type': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-    },
-  };
-  
+  },
+  rules: {
+    // 任意のルールをここに追加できます。
+    // 例: '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+};
