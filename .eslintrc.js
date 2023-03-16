b0d1ea9ef6e903ae159724fd48b4368a9b5caf60
ryosuke-horie <ryosuke03140@gmail.com>
2023-03-17 08:41:50 +0900

    eslintとPrettierの導入

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      // 任意のルールをここに追加できます。
+      // 例: '@typescript-eslint/explicit-function-return-type': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  };
+  
