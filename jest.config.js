c46496350799aa4225e16861bfa7e9916f57d034
ryosuke-horie <ryosuke03140@gmial.com>
2023-03-17 19:23:30 +0900

    indexページのテストを作成＆動作できるように設定を変更

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // この行を追加
+  moduleNameMapper: {
+    // 既存の設定を保持
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/next-env.d.ts',
+  ],
+  // その他の設定があればここに
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+    },
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
 };
