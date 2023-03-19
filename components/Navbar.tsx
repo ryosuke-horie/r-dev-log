4ccf98c79ddeb274c37408b268974597b0ecd6e9
ryosuke-horie <ryosuke03140@gmial.com>
2023-03-20 00:00:56 +0900

    ヘッダー用のナビゲーションバーを用意し、TailWindCSS用の設定を修正

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,39 +3,47 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="bg-blue-500 text-white shadow-lg py-4">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/" passHref>
-          サイト名 / ロゴ
+          <span className="text-xl font-bold cursor-pointer">
+            サイト名 / ロゴ
+          </span>
         </Link>
+        <ul className="flex space-x-6">
+          <li>
+            <Link href="/" passHref>
+              <span className="hover:text-blue-300 cursor-pointer">ホーム</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio" passHref>
+              <span className="hover:text-blue-300 cursor-pointer">
+                ポートフォリオ
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/skills" passHref>
+              <span className="hover:text-blue-300 cursor-pointer">
+                スキルセット
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog" passHref>
+              <span className="hover:text-blue-300 cursor-pointer">ブログ</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" passHref>
+              <span className="hover:text-blue-300 cursor-pointer">
+                お問い合わせ
+              </span>
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link href="/" passHref>
-            ホーム
-          </Link>
-        </li>
-        <li>
-          <Link href="/portfolio" passHref>
-            ポートフォリオ
-          </Link>
-        </li>
-        <li>
-          <Link href="/skills" passHref>
-            スキルセット
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog" passHref>
-            ブログ
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" passHref>
-            お問い合わせ
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 };
